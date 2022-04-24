95f924202a37a5633ab1d8eb385f95b65778ca16
Kacper <spiderpl1@o2.pl>
2022-04-24 16:15:21 +0200

    Add AUTH_USER_MODEL into settings.py

--- final_project/final_project/settings.py
+++ final_project/final_project/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTH_USER_MODEL = 'snapvisit.Profile'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
