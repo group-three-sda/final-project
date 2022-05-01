3445470840ca373cc988c08a3b2be3d443101a72
Dariusz Klimowicz <dariusz.secop@gmail.com>
2022-05-01 15:15:13 +0200

    Configure Home Page path

--- final_project/snapvisit/urls.py
+++ final_project/snapvisit/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from snapvisit.views import HomePageView
+
+app_name = 'snapvisit'
 
 urlpatterns = [
-
+    path('', HomePageView.as_view(), name='home-page')
 ]
